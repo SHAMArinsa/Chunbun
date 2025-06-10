13d559b1fa59778a78eae138279cac7f25270910
SHAMArinsa <Shampachak01@gmail.com>
2025-06-11 02:44:10 +0530

    Update about-us.tsx

--- frontend/src/pages/about-us.tsx
+++ frontend/src/pages/about-us.tsx
@@ -1,6 +1,6 @@
 // src/pages/about-us.tsx
 
-import React from 'react';
+//import React from 'react';
 import AboutUsPage from '../components/AboutUs/AboutUsPage';
 
 const AboutUs = () => <AboutUsPage />;
