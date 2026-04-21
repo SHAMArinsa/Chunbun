ff785554cd35787db7b3063dc6a0721436b0c800
SHAMArinsa <Shampachak01@gmail.com>
2026-04-21 14:18:11 +0530

    Update index.tsx

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import HomePageContent from '../components/HomePage/HomePageContent';
+import { Helmet } from "react-helmet-async";
 <link rel="icon" type="image/jpeg" href="/images/logo.jpeg" />
 
 
@@ -10,4 +11,14 @@ const HomePage = () => {
   );
 };
 
+
+<Helmet>
+  <title>Arinsa AI Minds | Generative AI & Automation Company</title>
+  <meta 
+    name="description" 
+    content="Arinsa AI Minds provides AI automation, NLP, and LLM-based solutions to scale modern businesses." 
+  />
+</Helmet>
+
 export default HomePage;
+
