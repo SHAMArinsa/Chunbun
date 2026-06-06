35306ff37bef77ad5833b4000eda9a79adba9eca
SHAMArinsa <Shampachak01@gmail.com>
2026-06-06 20:49:51 +0530

    Delete frontend directory

--- frontend/src/pages/about-us.tsx
+++
@@ -1,8 +0,0 @@
-// src/pages/about-us.tsx
-
-//import React from 'react';
-import AboutUsPage from '../components/AboutUs/AboutUsPage';
-
-const AboutUs = () => <AboutUsPage closeDropdown={() => {}} />;
-
-export default AboutUs;
